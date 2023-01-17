b6fd83d9ae57c46971c2d0c7979f48a28504fbb0
Victor <duprezvictor4@gmail.com>
2023-01-17 23:30:16 +0100

    il est pas encore 23h59

--- exo_14.rb
+++ exo_14.rb
@@ -1,10 +1,8 @@
 Tableau = Array.new(50)
 
 for num in 1..50 do
-    if num < 10 
+    if num%2==0
         Tableau = Tableau + ["jean.dupont.0#{num}@email.fr"]
-    else
-        Tableau = Tableau + ["jean.dupont.#{num}@email.fr"]
     end
 end
 
